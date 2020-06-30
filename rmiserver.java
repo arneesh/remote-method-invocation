997b1f88496e836cdeb7e39fad9efdc0dfd1b188
Arneesh Aima <44923038+arneesh@users.noreply.github.com>
2020-06-30 12:09:23 +0530

    Update rmiserver.java

--- rmiserver.java
+++ rmiserver.java
@@ -6,17 +6,15 @@ import java.rmi.registry.*;
 import java.net.*;
 public class rmiserver
 {
-public static void main(String args[]) throws RemoteException
-{
-try
-{
-implement i=new implement();
-Naming.rebind("server",i);
-System.out.println("server ready");
-}
-catch(Exception e)
-{
-System.out.println("exception:"+e);
-}
-}
+    public static void main(String args[]) throws RemoteException
+    {
+        try{
+            implement i=new implement();
+            Naming.rebind("server",i);
+            System.out.println("server ready");
+        }
+        catch(Exception e){
+            System.out.println("exception:"+e);
+        }
+    }
 }
